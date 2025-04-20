7dd91ed0074b2211a5bf91497f73ec8f0d8c9e6c
Ndumiso Mlambo <nmlambotech@gmail.com>
2025-04-20 12:00:53 +0200

    More project configuration and added global styles and type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Job {
+    id: string
+    title: string
+    company: string
+    location: string
+    type: string
+    salary: string
+    description: string
+    requirements: string[]
+    responsibilities: string[]
+    postedAt: string
+    deadline: string
+  }
